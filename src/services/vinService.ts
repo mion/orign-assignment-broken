59c7cd08662ac55f0c6c53e2ee60332c441cca24
Gabriel Vieira <gluisvieira@gmail.com>
2018-11-06 19:03:36 -0300

    Add undef, null, empty and invalid checks to vinService convert func to make tests pass (and also return CarInfo stub)

--- src/services/vinService.ts
+++ src/services/vinService.ts
@@ -5,6 +5,27 @@ export const filter = (vin: string) => vin
 
 export const validate = (_vin: string): string => null
 
-export const convert = (_res: VinCheckResponse): CarInfo => null
+export const convert = (_res: VinCheckResponse): CarInfo => {
+    const isUndef = typeof _res === "undefined"
+    const isNull = _res == null
+    if (isUndef || isNull) {
+        return null
+    }
+    const isInvalid = typeof _res.Results === "undefined"
+    if (isInvalid) {
+        return null
+    }
+    const isEmpty = _res.Results.length === 0
+    if (isEmpty) {
+        return null
+    }
+    return {
+        make: "foo",
+        model: "bar",
+        year: 0,
+        trim: "quux",
+        vechicleType: "baz"
+    }
+}
 
 export const apiCheck = async (_vin: string): Promise<CarInfo> => null
